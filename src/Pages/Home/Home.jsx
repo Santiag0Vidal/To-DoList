91ff923e48402b948e99358f343a1e433a2d8d35
Santiag0Vidal <david.vidal@est.fi.uncoma.edu.ar>
2024-04-22 09:12:50 -0300

    mas cambios

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Tarea from '../../Components/Tarea/Tarea';
 import Titulo from '../../Components/Title/Title';
+import styles from './Home.module.css';
+
 function Home() {
     return (
-        <div>
+        <div className={styles.Limitado}>
             <Tarea></Tarea>
             <Titulo texto="Somos el grupo X2"/>
-
         </div>
     );
 }
+
 export default Home;
